11984ac277f72a0f56c0fab609d3fca6d81d769f
Robin BLANCHARD <robin.blanchard@telecom-paristech.fr>
2017-06-23 15:53:10 +0200

    Ajout de nouveau matcher et de nouveau template pour le niveau 1. Index chargé sur le niveau 1

---
+++ Monuments/matcher_level1.js
@@ -0,0 +1,5 @@
+var predicateMatchers=[
+    {matcherName: "rdfschema_label", predicate: "http://www.w3.org/2000/01/rdf-schema#label", templateVariable: "title", abstractTemplate: "title", order: 1100000 },
+    {matcherName: "depiction", predicate: "http://xmlns.com/foaf/0.1/depiction", templateVariable: "img_url", abstractTemplate: "img", order: 1000000 },
+    {matcherName: "abstract", predicate: "http://dbpedia.org/ontology/abstract", templateVariable: "abstract", abstractTemplate: "abstract", order: 900000 },
+];
