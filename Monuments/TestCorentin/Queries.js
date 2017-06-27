1315959479245a3f048ba6f05497b0ef7623ebe0
Corry <cocorobineau@gmail.com>
2017-06-27 13:37:38 +0200

    Correction itération de 10k en 10k plus ajout de regexp pour réduire la taille des résultats

--- Monuments/TestCorentin/Queries.js
+++ Monuments/TestCorentin/Queries.js
@@ -5,7 +5,7 @@ var labels = {places : 0,
 			  resourceHistoricalPlaces : 2,
 			  resourceWorldHeritageSites : 3,
 			  resourceArchitecturalStructures : 4,
-			  combinedMonumentsAndWorldHeritage : 5};
+			  combinedQuery : 5};
 			  
 // interesting queries for RDF dbpedia
 var dbpediaQueries = new Array(new Array("","?s","rdfs:subClassOf","<http://dbpedia.org/ontology/Place>",10000),
@@ -13,12 +13,13 @@ var dbpediaQueries = new Array(new Array("","?s","rdfs:subClassOf","<http://dbpe
 							   new Array("distinct ?s","?s","<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>","<http://dbpedia.org/ontology/HistoricPlace>",10000),
 							   new Array("distinct ?s","?s","<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>","<http://dbpedia.org/ontology/WorldHeritageSite>",10000),
 							   new Array("distinct ?s","?s","<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>","<http://dbpedia.org/ontology/ArchitecturalStructure>",10000),
-							   new Array("?s","?s","<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>","?o.",20000)
+							   new Array("?s","?s","<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>","?o.",10000)
 					);			  
 
-function make_combined_filter(URI1,URI2,URI3,URI4)
+					
+function make_combined_filter(URI1,URI2,URI3,URI4,regexp)
 {
-	var resultFilter = " filter (?o = " + URI1;
+	var resultFilter = " filter ((?o = " + URI1;
 	
 	if(URI2 != null)
 		resultFilter = resultFilter.concat(" || ?o = " + URI2);
@@ -29,8 +30,22 @@ function make_combined_filter(URI1,URI2,URI3,URI4)
 	if(URI4 != null)
 		resultFilter = resultFilter.concat(" || ?o = " + URI4);
 	
-	resultFilter = resultFilter.concat(" )");
+	if(regexp.localeCompare("") != 0)
+		resultFilter = resultFilter.concat(" ) && " + regexp + " )");
+	else
+		resultFilter = resultFilter.concat(" ))");
 	
 	return resultFilter;
-}				
+}
+
+function make_regexp_filter(regexp)
+{
+	return " filter ( " + regexp + " )";
+}
+
+function make_regexp(variable,regexp,option)
+{
+	return "regex(" + variable + ",\""+ regexp + "\",\"" + option + "\")";
+}
+
 /**************************************************************/			
